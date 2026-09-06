682b703c64142857e9611723e30792d403e668ea
TooMuchFreeTime44 <jrkeliher@gmail.com>
2026-09-05 22:09:55 -0500

    Got buttons working and polished

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /// <reference types="p5/global" />
 class Button {
-    constructor(x, y, w, h, rad, strokeAmt, bodyCol, lineCol, message, blackText, boldText) {
+    constructor(x, y, w, h, rad, strokeAmt, bodyCol, lineCol, message, textHeight, blackText, boldText) {
         this.x = x;
         this.y = y;
         this.w = w;
@@ -10,6 +10,7 @@ class Button {
         this.bodyCol = bodyCol;
         this.lineCol = lineCol;
         this.message = message;
+        this.textHeight = textHeight;
         this.blackText = blackText;
         this.boldText = boldText;
     }
@@ -32,8 +33,9 @@ class Button {
             textStyle(NORMAL);
         }
         noStroke();
+        textSize(this.textHeight);
         textAlign(CENTER, CENTER);
-        text(this.message, 0, 2);
+        text(this.message, 0, 5);
         pop();
     }
 
@@ -43,24 +45,32 @@ class Button {
 }
 
 let state = "ready";
+let canPressButton = true;
+let buttonStroke = 12;
 let startButton;
+let pauseButton;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    startButton = new Button(width / 2, height * 5 / 8, width, width, height / 6, 70, 8, color(0, 140, 200), color(0, 120, 170), "Start", false, true);
+    startButton = new Button(width / 2, height * 5 / 8, width - buttonStroke, height / 6, 70, buttonStroke, color(0, 140, 200), color(0, 120, 170), "Start", height / 10, false, true);
+    pauseButton = new Button(width / 2, height * 5 / 8, width - buttonStroke, height / 6, 30, buttonStroke, color(255, 55, 90), color(200, 50, 88), "Stop", height / 10, false, true);
 }
 
 function draw() {
-    background(50);
+    background(30, 50, 55);
     if (state === "ready") {
         startButton.show();
-        if (startButton.isPressed()) {
+        if (startButton.isPressed() && canPressButton) {
+            canPressButton = false;
             state = "timing";
         }
     }
     if (state === "timing") {
-        fill(255, 55, 90);
-        rect(0, height / 2, width, height / 6, 30);
+        pauseButton.show();
+        if (pauseButton.isPressed() && canPressButton) {
+            canPressButton = false;
+            state = "ready";
+        }
     }
     if (state === "waiting") {
 
@@ -69,5 +79,14 @@ function draw() {
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-    startButton = new Button(width / 2, height * 5 / 8, width, width, height / 6, 70, 8, color(0, 140, 200), color(0, 120, 170), "Start", false, true);
+    startButton = new Button(width / 2, height * 5 / 8, width - buttonStroke, height / 6, 70, buttonStroke, color(0, 140, 200), color(0, 120, 170), "Start", height / 10, false, true);
+    pauseButton = new Button(width / 2, height * 5 / 8, width - buttonStroke, height / 6, 30, buttonStroke, color(255, 55, 90), color(200, 50, 88), "Stop", height / 10, false, true);
+}
+
+function mousePressed() {
+    return false;
+}
+
+function mouseReleased() {
+    canPressButton = true;
 }
