a52711e4615d3d9428d50fa5af05caac8d9ee9f1
TooMuchFreeTime44 <jrkeliher@gmail.com>
2026-09-05 20:34:46 -0500

    Begun adding basic buttons for state switching, though buggy

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /// <reference types="p5/global" />
 class Button {
-    constructor(x, y, w, h, rad, strokeAmt, bodyCol, lineCol, message, blackText) {
+    constructor(x, y, w, h, rad, strokeAmt, bodyCol, lineCol, message, blackText, boldText) {
         this.x = x;
         this.y = y;
         this.w = w;
@@ -11,19 +11,25 @@ class Button {
         this.lineCol = lineCol;
         this.message = message;
         this.blackText = blackText;
+        this.boldText = boldText;
     }
     
     show() {
         push();
         translate(this.x, this.y);
-        fill(bodyCol);
-        strokeWeight(strokeAmt);
-        stroke(lineCol);
+        fill(this.bodyCol);
+        strokeWeight(this.strokeAmt);
+        stroke(this.lineCol);
         rect(-this.w / 2, -this.h / 2, this.w, this.h, this.rad);
         if (this.blackText) {
             fill(0);
         } else {
             fill(255);
+        }
+        if (this.boldText) {
+            textStyle(BOLD);
+        } else {
+            textStyle(NORMAL);
         }
         noStroke();
         textAlign(CENTER, CENTER);
@@ -41,14 +47,16 @@ let startButton;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    startButton = new Button();
+    startButton = new Button(width / 2, height * 5 / 8, width, width, height / 6, 70, 8, color(0, 140, 200), color(0, 120, 170), "Start", false, true);
 }
 
 function draw() {
     background(50);
     if (state === "ready") {
-        fill(0, 140, 200);
-        rect(0, height / 2, width, height / 6, 70);
+        startButton.show();
+        if (startButton.isPressed()) {
+            state = "timing";
+        }
     }
     if (state === "timing") {
         fill(255, 55, 90);
@@ -61,4 +69,5 @@ function draw() {
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+    startButton = new Button(width / 2, height * 5 / 8, width, width, height / 6, 70, 8, color(0, 140, 200), color(0, 120, 170), "Start", false, true);
 }
